77b5f0b7b36ded043952fd863a04ea04fff775e6
Jean Oliveira <jcwilbertt@gmail.com>
2023-09-23 13:55:36 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Post } from "./Post"
+
+function App() {
+  return (
+    <>
+      <Post 
+        author="Jean Oliveira"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo iure et eum earum, unde corporis. Totam labore deserunt doloribus corrupti qui dolores ducimus. Quasi nam omnis reiciendis quo. Ullam, sapiente!" 
+      />
+
+      <Post
+        author="Johan Oliveira"
+        content="Sou chato"
+      />
+    </>
+  )
+}
+
+export default App
+
